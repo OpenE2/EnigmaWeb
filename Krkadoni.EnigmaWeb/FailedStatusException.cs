b510c6472d97532c2e261f7547f9192d15ae564c
shaxxx <isakota@gmail.com>
2015-12-21 15:38:37 +0100

    Added FailedStatusException

---
+++ Krkadoni.EnigmaWeb/FailedStatusException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace Krkadoni.Enigma
+{
+    public class FailedStatusCodeException : KnownException
+    {
+        public HttpStatusCode StatusCode { get; private set;}
+
+        public FailedStatusCodeException(string message, HttpStatusCode statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+            
+        public FailedStatusCodeException(string message, HttpStatusCode statusCode, Exception innerException) : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
